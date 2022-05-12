b463a7f0c19c1bf5d341c6913f44a71be6c72875
rodrigo-guzman <rodrigogastonguzman@gmail.com>
2022-05-12 17:30:00 -0300

    mejoras

--- src/components/navbar-desktop/navbar-desktop.jsx
+++ src/components/navbar-desktop/navbar-desktop.jsx
@@ -4,62 +4,59 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import CartWidget from '../cartwidget';
 import { red } from '@mui/material/colors';
 
-const color = red[500];
-
-
 function navbardesktop({nombre, logo}){
-
+    const color = red[500];
     return <>
-    <IconButton sx={{ p: 0, display: { xs: 'none', md: 'flex' } }}>
-            <img width={50} height={50} src={logo} alt="Ecommerce" />
-          </IconButton>
+      <IconButton sx={{ p: 0, display: { xs: 'none', md: 'flex' } }}>
+        <img width={50} height={50} src={logo} alt="Ecommerce" />
+      </IconButton>
 
-          <Typography
-            variant="h4"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              display: { xs: 'none', md: 'flex' },
-              textDecoration: 'none',
-              color: '#ffffff'
-            }}
-          >
-          {nombre}
-          </Typography>
+      <Typography
+        variant="h4"
+        noWrap
+        component="a"
+        href="/"
+        sx={{
+          display: { xs: 'none', md: 'flex' },
+          textDecoration: 'none',
+          color: '#ffffff'
+        }}
+      >
+        {nombre}
+      </Typography>
 
-          <Box sx={{ flexGrow: 1, justifyContent: 'center', alignContent: 'center', gap: 4, display: { xs: 'none', md: 'flex' } }}>
-            <Button
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              Vehículos
-            </Button>
-            <Button
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              Electrónica
-            </Button>
-            <Button
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              Libros
-            </Button>
-          </Box>
+      <Box sx={{ flexGrow: 1, justifyContent: 'center', alignContent: 'center', gap: 4, display: { xs: 'none', md: 'flex' } }}>
+        <Button
+          sx={{ my: 2, color: 'white', display: 'block' }}
+        >
+          Vehículos
+        </Button>
+        <Button
+          sx={{ my: 2, color: 'white', display: 'block' }}
+        >
+          Electrónica
+        </Button>
+        <Button
+          sx={{ my: 2, color: 'white', display: 'block' }}
+        >
+          Libros
+        </Button>
+      </Box>
+      
+      <Button color='secondary' sx={{display: { xs: 'none', md: 'flex' } }}>
+            <CartWidget qtyItems={4}/>
+      </Button> 
+      
+      <Box sx={{ justifyContent: 'flex-end', alignContent: 'center', gap: 4, display: { xs: 'none', md: 'flex', paddingLeft: '3rem'} }}>
+        <Button
+          variant='contained'
           
-          <Button color='secondary' sx={{display: { xs: 'none', md: 'flex' } }}>
-                <CartWidget qtyItems={4}/>
-          </Button> 
-          
-          <Box sx={{ justifyContent: 'flex-end', alignContent: 'center', gap: 4, display: { xs: 'none', md: 'flex', paddingLeft: '3rem'} }}>
-            <Button
-              variant='contained'
-              
-              sx={{ display: 'flex', backgroundColor: color}}
-              startIcon={<AccountCircleIcon />}
-            >
-            Loguin
-            </Button>
-          </Box>
+          sx={{ display: 'flex', backgroundColor: color}}
+          startIcon={<AccountCircleIcon />}
+        >
+        Loguin
+        </Button>
+      </Box>
 
     </> ;
 }
--- src/components/navbar-mobile/navbar-mobile.jsx
+++ src/components/navbar-mobile/navbar-mobile.jsx
@@ -5,39 +5,39 @@ import CartWidget from '../cartwidget';
 
 function NavBarMobile({nombre, logo}){
     return <>
-     <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-            sx={{ display: { xs: 'flex', md: 'none' } }}
-          >
-          <MenuIcon />
-          </IconButton>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        color="inherit"
+        sx={{ display: { xs: 'flex', md: 'none' } }}
+      >
+        <MenuIcon />
+      </IconButton>
           
-          <Box sx={{ display: { xs: 'flex', md: 'none' }, flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <IconButton sx={{ p: 0 }}>
-              <img width={60} height={60} src={logo} alt="Ecommerce" />
-            </IconButton>
+      <Box sx={{ display: { xs: 'flex', md: 'none' }, flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <IconButton sx={{ p: 0 }}>
+          <img width={60} height={60} src={logo} alt="Ecommerce" />
+        </IconButton>
 
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                color: '#ffffff',
-                textDecoration: 'none',
-              }}
-            >
-              ECOMMERCE
-            </Typography> 
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            color: '#ffffff',
+            textDecoration: 'none',
+          }}
+        >
+          ECOMMERCE
+        </Typography> 
 
-            <Button color='secondary' sx={{justifyContent: 'right', alignContent: 'right', paddingLeft: '4rem'}}>
-                <CartWidget qtyItems={4}/>
-            </Button>        
-          </Box>
+        <Button color='secondary' sx={{justifyContent: 'right', alignContent: 'right', paddingLeft: '4rem'}}>
+            <CartWidget qtyItems={4}/>
+        </Button>        
+      </Box>
     </> ;
 }
 
