88134179c89317f07438d592c7db7343f77b072f
rodrigo-guzman <rodrigogastonguzman@gmail.com>
2022-05-29 00:38:18 -0300

    clase 9 context fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import ItemDetailContainer from './components/ItemDetailContainer';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Cart from './components/Cart';
 import ContextCartProvider from './contexts/ContextCart';
-import FirebaseDoc from './components/FirebaseDoc';
 import './App.css';
-import FirebaseDocs from './components/FirebaseDocs';
 
 function App() {
 
@@ -19,8 +17,6 @@ function App() {
           <Route path="/" element = {<ItemListContainer mensaje = {"CATALOGO"}/>}></Route>
           <Route path="/category/:categoryId" element = {<ItemListContainer mensaje = {"CATALOGO"}/>}></Route>
           <Route path="/ItemDetailContainer/:idProduct" element = {<ItemDetailContainer/>}></Route>
-          <Route path="/doc" element = {<FirebaseDoc />}></Route>
-          <Route path="/docs" element = {<FirebaseDocs />}></Route>
           <Route path="/cart" element = {<Cart/>}></Route>
           <Route path="*" element = {<h1>ERROR 404</h1>}></Route>
         </Routes>
