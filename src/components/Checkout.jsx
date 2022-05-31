86bb27b3b298455ac6d93ac291fcb172a14c61c9
rodrigo-guzman <rodrigogastonguzman@gmail.com>
2022-05-31 16:52:14 -0300

    avances entrega final

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,60 +2,11 @@ import { Button, Container, TextField, Typography } from "@mui/material";
 import React, { useContext, useState } from "react";
 import { contextCart } from '../contexts/ContextCart';
 import { collection, getFirestore, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
 
-// A custom validation function. This must return an object
-// which keys are symmetrical to our values/initialValues
-const validate = values => {
-    const errors = {};
-  
-    if (!values.firstName) {
-      errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-      errors.firstName = 'Must be 15 characters or less';
-    }
-  
-    if (!values.lastName) {
-      errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-      errors.lastName = 'Must be 20 characters or less';
-    }
-  
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
-    }
-  
-    return errors;
-  };
 
 export const Checkout = () => {
     const { cart, totalPrice } = useContext(contextCart);
     const [id, setId] = useState("");    
-    const [values, setValues] = React.useState({});
-
-
-    const formik = useFormik({
-        initialValues: {
-          name: '',
-          email: '',
-          phone: '',
-        },
-        validationSchema: Yup.object({
-          name: Yup.string()
-            .max(15, 'Must be 15 characters or less')
-            .required('Required'),
-          email: Yup.string().email('Invalid email address').required('Required'),
-          phone: Yup.string()
-          .max(20, 'Must be 20 characters or less')
-          .required('Required'),
-        }),
-        onSubmit: values => {
-          alert(JSON.stringify(values, null, 2));
-        },
-      });
 
     const [buyer, setbuyer] = useState({
         name: "",
